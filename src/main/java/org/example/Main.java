1aea01bd5d1c120daf7adc9ed217f6cee6eb3d32
Trizie <patrizia.stutzenstein@gmx.at>
2024-07-29 12:47:34 +0200

    Refactoring

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,28 +1,35 @@
 package org.example;
+import java.util.List;
 import java.util.Scanner;
 import java.util.ArrayList;
 
-List<String> repice_list = new ArrayList();
+
 
 public class Main {
-    Scanner recipeScanner = new Scanner(System.in);
+
+    private List<String> recipe_list;
+
 
     static void search_for_recipe() {
-        System.out.println(recipe_list);
+        Scanner addRecipeScanner = new Scanner(System.in);
+        System.out.println("Tippen Sie das gesuchte Rezept ein");
+        String recipe = addRecipeScanner.nextLine();
     }
 
     static void add_recipe_to_list() {
         Scanner addRecipeScanner = new Scanner(System.in);
         System.out.println("Recipe is added to list");
-        System.out.println("Insert the recipe name:");
+        System.out.println("Rezeptname eintragen:");
         String recipeName = addRecipeScanner.nextLine();
-        System.out.println("Insert the ingredients needed:");
+        System.out.println("Benoetigte Zutaten eintragen:");
         String ingredients = addRecipeScanner.nextLine();
-        
+        System.out.println("Anleitung eintragen:");
+        String instructions = addRecipeScanner.nextLine();
+
     }
 
     static void print_recipe_list() {
-        System.out.println(recipe_list);
+        System.out.println("Rezeptliste");
     }
     public static void main(String[] args) {
         Scanner recipeScanner = new Scanner(System.in);
@@ -31,17 +38,25 @@ public class Main {
         System.out.println("a) Alle Rezepte auflisten");
         System.out.println("b) Ein Rezept hinzufuegen");
         System.out.println("c) Nach einem Rezept suchen");
+        System.out.println("d) Programmende");
         String chooseFunction = recipeScanner.nextLine();
         System.out.println(chooseFunction);
 
-        if (chooseFunction=="a"){
-            print_recipe_list();
-        }
-        else if (chooseFunction=="b") {
-            add_recipe_to_list();
-        }
-        else if (chooseFunction=="c") {
-            search_for_recipe();
+        switch(chooseFunction){
+            case "a":
+                print_recipe_list();
+                break;
+            case "b":
+                add_recipe_to_list();
+                break;
+            case "c":
+                search_for_recipe();
+                break;
+            case "d":
+                break;
+            default:
+                System.out.println("Ungueltig, bitte andere Auswahl");
+
         }
     }
 }
